7c6ed20c681aebfbe15072689659594523925e55
jstar <drealjstar@yahoo.com>
2021-07-07 14:48:24 -0700

    adjusting code

--- src/Component/news.js
+++ src/Component/news.js
@@ -1,55 +1,55 @@
 import React from "react";
 import Axios from 'axios';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
-class News extends React.Component{
+class News extends React.Component {
   state = {
     articles: [],
   };
 
-componentDidMount(){
-  const country_code = this.props.match.params.country_code;
-  this.getArticles(country_code)
-}
+  componentDidMount() {
+    const country_code = this.props.match.params.country_code;
+    this.getArticles(country_code)
+  }
 
-getArticles = (country) => {
-  Axios.get(
-    `https://hd-news.herokuapp.com/news/${country}`
-  ).then((result) => {
-    this.setState({ articles: result.data.articles });
-  });
-};
+  getArticles = (country) => {
+    Axios.get(
+      `https://hd-news.herokuapp.com/news/${country}`
+    ).then((result) => {
+      this.setState({ articles: result.data.articles });
+    });
+  };
 
 
-render(){
+  render() {
     return (
       <div className="wrapper">
-        <div className = 'header'>
-        <h2> Headline News</h2>
+        <div className='header'>
+          <h2> Headline News</h2>
 
-        <Link to= "/tags">
-        <p className= "backclick">Back</p>
-        </Link>
+          <Link to="/tags">
+            <p className="backclick">Back</p>
+          </Link>
 
         </div>
-       {this.state.articles.length < 1 ? (<h2 className='loading'>Loading ...</h2>) :
-       
-       <div className="cards">
-       {this.state.articles.map((article, i) => {
-         return (
-           <div className="card" key={i}>
-             <h3>{article.author ? article.author : 'No author'}</h3>
-             <img src={article.urlToImage} alt="No" />
+        {this.state.articles.length < 1 ? (<h2 className='loading'>Loading ...</h2>) :
 
-             <p>{article.source.name}</p>
-             <p>{article.published}</p>
-             <p>{article.title}</p>
-           </div>
-         );
-       })}
-       </div>
-       }
+          <div className="cards">
+            {this.state.articles.map((article, i) => {
+              return (
+                <div className="card" key={i}>
+                  <h3>{article.author ? article.author : 'No author'}</h3>
+                  <img src={article.urlToImage} alt="No" />
+
+                  <p>{article.source.name}</p>
+                  <p>{article.published}</p>
+                  <p>{article.title}</p>
+                </div>
+              );
+            })}
+          </div>
+        }
       </div>
     );
   }
--- src/Component/tag.js
+++ src/Component/tag.js
@@ -3,12 +3,12 @@ import "./tag.css";
 import { Link } from "react-router-dom";
 
 class Tags extends Component {
-  
+
   render() {
 
     return (
       <div className="flag">
-        
+
         <h1 className="flag-header">COUNTRIES HEADLINE NEWS</h1>
 
         <div className="countries">
@@ -17,24 +17,24 @@ class Tags extends Component {
               <h2>NIGERIA</h2>
             </div>
           </Link>
-          
+
           <Link to="/news/us" >
 
-          <div className="USA country">
-            <h2>USA</h2>
-          </div>
-        </Link>
+            <div className="USA country">
+              <h2>USA</h2>
+            </div>
+          </Link>
 
-        <Link to="/news/gb">
-          <div className="ENG country" >
-            <h2>UNITED KINGDOM</h2>
-          </div>
+          <Link to="/news/gb">
+            <div className="ENG country" >
+              <h2>UNITED KINGDOM</h2>
+            </div>
           </Link>
 
           <Link to="/news/ar">
-          <div className="ARG country" >
-            <h2>ARGENTINA</h2>
-          </div>
+            <div className="ARG country" >
+              <h2>ARGENTINA</h2>
+            </div>
           </Link>
 
         </div>
